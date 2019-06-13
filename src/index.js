5b9c49c91ae4a57cabe2ddd3cf8585e80c8244ea
3014zhangshuo <3014zhangshuo@gmail.com>
2019-06-13 23:17:12 +0800

    渲染列表数据

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React, { Component } from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 
+const users = [
+  { username: 'Jerry', age: 21, gender: 'male' },
+  { username: 'Tomy', age: 22, gender: 'male' },
+  { username: 'Lily', age: 19, gender: 'female' },
+  { username: 'Lucy', age: 20, gender: 'female' }
+]
+
 class LikeButton extends Component {
   static defaultProps = {
     likedText: "取消",
@@ -78,34 +85,25 @@ class Footer extends Component {
   }
 }
 
+class User extends Component {
+  render() {
+    const { user } = this.props
+    return(
+      <div>
+        <div>Name: {user.name}</div>
+        <div>Age: {user.age}</div>
+        <div>Gender: {user.gender}</div>
+        <hr />
+      </div>
+    )
+  }
+}
+
 class Index extends Component {
-  constructor() {
-    super()
-    this.state = {
-      likedText: "已赞",
-      unlikedText: "赞"
-    }
-  }
-
-  handleClickOnChange() {
-    this.setState({
-      likedText: "取消",
-      unlikedText: "点赞"
-    })
-  }
-
   render() {
     return(
       <div>
-        <LikeButton 
-          likedText={this.state.likedText} unlikedText= {this.state.unlikedText}
-          onClick={() => console.log('Click on like button!')}/>
-        <div>
-          <button onClick={this.handleClickOnChange.bind(this)}>
-            修改 wordings
-          </button>
-        </div>
-        <HelloWorld />
+        {users.map((user, i) => <User key={i} user={user} />)}
       </div>
     )
   }
